1587398c859b05be47ba9ab26b3bda66e84fda4e
JRed <joseph@titaniumcoast.com>
2021-09-04 18:32:29 -0400

    Fixes and Added More Song Setup Stuff

--- source/Register.hx
+++ source/Register.hx
@@ -15,7 +15,7 @@ class Register {
     static function sendRegistration() {
         var parentWindow = Util.getParent();
 
-        parentWindow.postMessage({purpose: "register"}, "fnfcentral.com");
+        parentWindow.postMessage({purpose: "register"}, Util.getTargetURL());
     }
 
     static function listenForGameInfo() {
@@ -25,13 +25,13 @@ class Register {
     static function setData(event: MessageEvent) {
         trace("Message Recieved!  From:" + event);
 
-        if (event.origin != "http://fnfcentral.com" || event.origin != "https://fnfcentral.com")
+        if (event.origin != Util.getTargetURL())
             return;
 
-        if (event.data.purpose = "set-ID-data")
-
-        Info.songIDs = event.data.songIDs;
-        Info.difficultyIDs = event.data.difficultyIDs;
-        trace("Set Info From:" + event.data);
+        if (event.data.purpose = "set_ID_data") {
+            Info.songIDs = event.data.songIDs;
+            Info.difficultyIDs = event.data.difficultyIDs;
+            trace("Set Info From:" + event.data);
+        }
     }
 }
--- source/Status.hx
+++ source/Status.hx
@@ -7,7 +7,19 @@ import js.Browser;
 **/
 class Status {
     static public function Note(hit: Bool, currentScore: Int, combo: Int, accuracy: Float, song: String, diff: Int) {
-        Util.getParent().postMessage({purpose: "note-hit", hit: hit, currentScore: currentScore, combo: combo, accuracy: accuracy, timetamp: Date.now(), songID: calcSongID(song), diffID: calcDiffID(diff)}, Util.getTargetURL());
+        Util.getParent().postMessage({purpose: "note_hit", hit: hit, currentScore: currentScore, combo: combo, accuracy: accuracy, songID: calcSongID(song), diffID: calcDiffID(diff)}, Util.getTargetURL());
+    }
+
+    static public function Start(song: String, diff: Int) {
+        Util.getParent().postMessage({purpose: "song_start", songID: calcSongID(song), diffID: calcDiffID(diff)}, Util.getTargetURL());
+    }
+
+    static public function Pass(score: Int, accuracy: Float, song: String, diff: Int) {
+        Util.getParent().postMessage({purpose: "song_pass", songID: calcSongID(song), diffID: calcDiffID(diff)}, Util.getTargetURL());
+    }
+
+    static public function Fail(score: Int, accuracy: Float, song: String, diff: Int) {
+        Util.getParent().postMessage({purpose: "song_fail", score: score, accuracy: accuracy, songID: calcSongID(song), diffID: calcDiffID(diff)}, Util.getTargetURL());
     }
 
     static function calcSongID(song: String) {

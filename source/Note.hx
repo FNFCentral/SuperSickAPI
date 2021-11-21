cb6ec48d4ce89b9fecce59b948ea45f56a41f123
JRed <joseph@titaniumcoast.com>
2021-11-20 21:07:15 -0500

    Not Sure Why Haxe Sucks So Much

--- source/Note.hx
+++ source/Note.hx
@@ -1,10 +1,11 @@
 package;
 
-enum Note {
-    Basic;
-    Holy;
-    HolyInstakill;
-    Cursed;
-    CursedPoison;
-    CursedInstakill;
+@:enum
+abstract Note(Int) {
+    var Basic = 0;
+    var Holy = 1;
+    var HolyInstakill = 2;
+    var Cursed = 3;
+    var CursedPoison = 4;
+    var CursedInstakill = 5;
 }

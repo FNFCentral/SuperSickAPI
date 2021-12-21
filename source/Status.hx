89c0054dfcdda8946e62facff60ee2abf750606b
JRed <joseph@titaniumcoast.com>
2021-12-20 19:36:34 -0500

    Added Sustain Notes

--- source/Status.hx
+++ source/Status.hx
@@ -4,16 +4,20 @@ package;
  * The status manager.  
 **/
 class Status {
-    static public function State(currentScore: Int, currentTime: Float, health: Float, combo: Int, accuracy: Float, song: String, diff: Int) {
-        Util.sendMessage({purpose: "state", currentScore: currentScore, currentTime: currentTime, health: health, combo: combo, accuracy: accuracy, diffID: Util.calcDiffID(song, diff)});
+    static public function State(currentScore: Int, currentTime: Float, health: Float, combo: Int, accuracy: Float, song: String, diff: Int, notesPressed: Map<Int, Bool> = null) {
+        Util.sendMessage({purpose: "state", currentScore: currentScore, currentTime: currentTime, health: health, combo: combo, accuracy: accuracy, diffID: Util.calcDiffID(song, diff), notesPressed: notesPressed});
     }
 
-    static public function NoteHit(noteTime: Float, delay: Float, currentScore: Int, combo: Int, accuracy: Float, song: String, diff: Int, noteType: Int = 0) {
-        Util.sendMessage({purpose: "hit", noteTime: noteTime, noteType: noteType, delay: delay, currentScore: currentScore, combo: combo, accuracy: accuracy, diffID: Util.calcDiffID(song, diff)});
+    static public function NoteHit(noteTime: Float, delay: Float, currentScore: Int, combo: Int, accuracy: Float, song: String, diff: Int, noteType: Int = 0, lane: Int = 0) {
+        Util.sendMessage({purpose: "hit", noteTime: noteTime, noteType: noteType, delay: delay, currentScore: currentScore, combo: combo, accuracy: accuracy, diffID: Util.calcDiffID(song, diff), lane: lane});
     }
 
-    static public function NoteMiss(noteTime: Float, currentScore: Int, combo: Int, accuracy: Float, song: String, diff: Int, noteType: Int = 0) {
-        Util.sendMessage({purpose: "miss", noteTime: noteTime, noteType: noteType, currentScore: currentScore, combo: combo, accuracy: accuracy, diffID: Util.calcDiffID(song, diff)});
+    static public function NoteMiss(noteTime: Float, currentScore: Int, combo: Int, accuracy: Float, song: String, diff: Int, noteType: Int = 0, lane: Int = 0) {
+        Util.sendMessage({purpose: "miss", noteTime: noteTime, noteType: noteType, currentScore: currentScore, combo: combo, accuracy: accuracy, diffID: Util.calcDiffID(song, diff), lane: lane});
+    }
+
+    static public function SustainMiss(noteTime: Float, currentScore: Int, combo: Int, accuracy: Float, song: String, diff: Int, noteType: Int = 0, lane: Int = 0) {
+        Util.sendMessage({purpose: "sustain_miss", noteTime: noteTime, noteType: noteType, currentScore: currentScore, combo: combo, accuracy: accuracy, diffID: Util.calcDiffID(song, diff), lane: lane});
     }
 
     static public function Start(song: String, diff: Int) {
